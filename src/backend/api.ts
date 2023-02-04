a952fde4969e32369b08a3551fd6c81bc39db4f1
hopeful <hopeful_world@163.com>
2023-02-04 17:15:49 +0800

    change xhr to websocket

--- src/backend/api.ts
+++ src/backend/api.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import { ChatGPTAPI } from 'chatgpt';
 import express from 'express';
+import expressWs from 'express-ws';
 import morgan from 'morgan';
 
 dotenv.config();
@@ -12,7 +13,7 @@ if (!API_KEY) {
 const api = new ChatGPTAPI({ apiKey: API_KEY });
 
 
-const app = express();
+const app = expressWs(express()).app;
 const port = process.env.PORT || 3000;
 
 app.use(morgan(process.env.MORGAN_FORMAT || 'combined'));
@@ -22,8 +23,8 @@ app.use(express.json());
 app.set('views', './src/static');
 app.set('view engine', 'pug');
 app.get('/', (req, res) => {
-    res.render('index');
-})
+    res.render('ws');
+});
 
 app.post('/ask', async (req, res) => {
     const text = req.body.text;
@@ -34,8 +35,41 @@ app.post('/ask', async (req, res) => {
         parentMessageId: mid
     });
     res.json(answer);
-})
+});
+
+app.ws('/longask', (ws, req) => {
+    var cid: string | undefined = undefined;
+    var mid: string | undefined = undefined;
+    var text = '';
+    var handling = false;
+    ws.onmessage = async (msg) => {
+        if (handling) {
+            ws.send('Another message is processing!')
+            return;
+        }
+        text += msg.data;
+        if (text.endsWith('\n')) {
+            handling = true;
+            var offset = 0;
+            text = text.substring(0, text.length - 1);
+            let answer = await api.sendMessage(text, {
+                conversationId: cid,
+                parentMessageId: mid,
+                onProgress: (part) => {
+                    let newdata = part.text.substring(offset);
+                    offset += newdata.length;
+                    ws.send(newdata);
+                }
+            });
+            ws.send('\u0000');
+            cid = answer.conversationId;
+            mid = answer.id;
+            text = '';
+            handling = false;
+        }
+    }
+});
 
 app.listen(port, () => {
     console.log(`App start listen port ${port}`);
-})
+});
